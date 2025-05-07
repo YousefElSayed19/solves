b439521e7a3366a83c0fea7ae4dade109fd9c183
YousefElSayed19 <yousafe2042005@gmail.com>
2025-05-07 14:34:09 +0300

    first commit

---
+++ solve.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define Free_Palestine int main()
+#define loop  \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define sortStr(word) sort(word.begin(), word.end())
+#define sortArr(arr, n) sort(arr, arr + n)
+#define ll long long
+#define ld long double
+#define cot(word) cout << word << endl
+#define cit(word) \
+    int word;     \
+    cin >> word
+#define CHECK  \
+    if (check) \
+    {          \
+        YES;   \
+    }          \
+    else       \
+    {          \
+        NO;    \
+    }
+Free_Palestine
+{
+    loop{
+        cit(n);
+		cout << (n == 3 ? 3 : 2) << endl;
+    }
+}
