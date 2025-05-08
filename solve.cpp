347da8e329ad2f6fe766fdc9890453e9f92b2f21
YousefElSayed19 <yousafe2042005@gmail.com>
2025-05-08 11:47:23 +0300

    Collecting Coins

--- solve.cpp
+++ solve.cpp
@@ -31,14 +31,14 @@ Free_Palestine
 {
     loop{
 
-        cit(a);
-        cit(b);
-        cit(c);
-        cit(n);
-        if((a+b+c+n)%3 ==0){
-            YES;
-        }else{
-            NO;
-        }
+        int a[3], n;
+		cin >> a[0] >> a[1] >> a[2] >> n;
+		sort(a, a + 3);
+		n -= 2 * a[2] - a[1] - a[0];
+		if (n < 0 || n % 3 != 0) {
+			cout << "NO" << endl;
+		} else {
+			cout << "YES" << endl;
+		}
     }
 }
