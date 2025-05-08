9544f77c2dc9e5180eb4099df134902a26d0a30e
YousefElSayed19 <yousafe2042005@gmail.com>
2025-05-08 11:30:23 +0300

    Honest Coach

--- solve.cpp
+++ solve.cpp
@@ -30,6 +30,27 @@ using namespace std;
 Free_Palestine
 {
     loop{
-        
+
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int &x : a) {
+            cin >> x;
+        }
+
+        sort(a.begin(), a.end());
+
+        int result = a[n - 1] - a[0];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                result = min(result, a[j] - a[i]);
+            }
+        }
+
+        cout << result << endl;
+
     }
 }
