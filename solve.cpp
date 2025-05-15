9d3ea21dcfa9d59a3a7320c8df44d7364441a4e2
YousefElSayed19 <yousafe2042005@gmail.com>
2025-05-15 14:47:49 +0300

    ش

--- solve.cpp
+++ solve.cpp
@@ -31,11 +31,18 @@ Free_Palestine
 {
     loop
     {
-        string str;
-        cin >> str;
-        str.pop_back();
-        str.pop_back();
-        cout << str + "i" << '\n';
+       int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for(int i = 0;i < n;i++){
+            cin>>arr[i];
+        }
+        if(is_sorted(arr,arr+n) || k > 1){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
     }
     return 0;
 }
