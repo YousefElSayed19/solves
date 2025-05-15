e9fd25c407805119563a5e5cca3b03bc1ccef907
YousefElSayed19 <yousafe2042005@gmail.com>
2025-05-15 14:50:21 +0300

    a

--- solve.cpp
+++ solve.cpp
@@ -31,17 +31,20 @@ Free_Palestine
 {
     loop
     {
-        int n,k;
-        cin>>n>>k;
+        int n, k;
+        cin >> n >> k;
         int arr[n];
-        for(int i = 0;i < n;i++){
-            cin>>arr[i];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
         }
-        if(is_sorted(arr,arr+n) || k > 1){
-            cout<<"YES\n";
+        if (is_sorted(arr, arr + n) || k > 1)
+        {
+            cout << "YES\n";
         }
-        else{
-            cout<<"NO\n";
+        else
+        {
+            cout << "NO\n";
         }
     }
     return 0;
