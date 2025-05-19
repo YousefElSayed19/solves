8bc1f2e8b2a5da18cf39ab46e5ded656fb838071
YousefElSayed19 <yousafe2042005@gmail.com>
2025-05-19 12:04:40 +0300

    a

--- solve.cpp
+++ solve.cpp
@@ -36,8 +36,8 @@ Free_Palestine
         int prev = 0, ans = 0;
         for (int i = 0; i < n; ++i) {
             int a; cin >> a;
+            ans = max(ans, a - prev);
     }
-        ans = max(ans, a - prev);
         prev = a;
         }
         ans = max(ans, 2 * (x - prev));
