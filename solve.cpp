bde444dbf6b21c5a5e74136851c4adcfb09d6266
YousefElSayed19 <yousafe2042005@gmail.com>
2025-05-20 03:03:32 +0300

    a

--- solve.cpp
+++ solve.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define Free_Palestine int main()
 #define ll long long
 #define loop  \
@@ -27,13 +26,25 @@ using namespace std;
     {          \
         NO;    \
     }
+
+void solve(){
+    cit(w);
+    cit(h);
+    cit(n);
+    ll sum = 1;
+    while(w%2 == 0){
+        w/=2;
+        sum*=2;
+    }
+    while(w%2 == 0){
+        w/=2;
+        sum*=2;
+    }
+}
 Free_Palestine
 {
     loop{
-        cit(w);
-        cit(h);
-        cit(n);
-        
+        solve()
     }
     return 0;
 }
