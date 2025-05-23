1c4f7bf2d75719991a1b00a15186741a81e81470
YousefElSayed19 <yousafe2042005@gmail.com>
2025-05-23 15:52:48 +0300

    a

--- solve.cpp
+++ solve.cpp
@@ -30,22 +30,6 @@ using namespace std;
 void solve()
 {
     cit(n);
-    cit(k);
-    int arr[n];
-    forn(i,n){
-        cin>>arr[i];
-    }
-    int sum=0;
-    int c = 0;
-    forn(i,n){
-        if(arr[i]>=k){
-            sum+=arr[i];
-        }else if(arr[i]==0 && sum>0){
-            sum--;
-            c++;
-        }
-    }
-    cot(c);
 }
 Free_Palestine
 {
